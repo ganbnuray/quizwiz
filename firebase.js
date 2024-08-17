0efc202712672f9ab8b5c13a25e2044ea0833e91
Ganbarova Nuray <ganbnuray@gmail.com>
2024-08-17 21:27:55 +0400

    firebase configuration edit

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHRNHsnAD8xEn3cBFxsIyfEowuHMhWthE",
-  authDomain: "flashcard-saas1.firebaseapp.com",
-  projectId: "flashcard-saas1",
-  storageBucket: "flashcard-saas1.appspot.com",
-  messagingSenderId: "301077309810",
-  appId: "1:301077309810:web:f155c3bc241383cd8215eb",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
